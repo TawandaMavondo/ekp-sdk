9218a1f768d10d088057c280fee7777fb091c8eb
gavin-shaw <gavin@gnarus.io>
2022-04-01 08:26:34 +0200

    feat: add onSubmit and multiRecord to Form

--- src/ui/components/form.component.ts
+++ src/ui/components/form.component.ts
@@ -2,6 +2,12 @@ import { DefaultProps } from '../default-props';
 import { Rpc } from '../rpc';
 import { UiElement } from '../ui-element';
 
+/**
+ * Creates an Earn Keeper Protocol object with the given properties
+ *
+ * @param props component properties
+ * @returns a ekp component object
+ */
 export function Form(props: FormProps): UiElement {
   return {
     _type: 'Form',
@@ -9,8 +15,36 @@ export function Form(props: FormProps): UiElement {
   };
 }
 
-export interface FormProps extends DefaultProps {
-  readonly name: Rpc | string;
-  readonly schema: Record<string, any>;
-  readonly child: UiElement;
-}
+/**
+ * Properties for the {@link Form} component
+ */
+export type FormProps = DefaultProps &
+  Readonly<{
+    /**
+     * @description the child components of this form
+     */
+    children: UiElement[];
+    /**
+     * @todo
+     */
+    multiRecord?: MultiRecordConfig;
+    /**
+     * @description the name of the form, can be referenced later
+     */
+    name: Rpc | string;
+    /**
+     * @description an {@link Rpc} object to be run when the form is submitted
+     * @default undefined
+     * @example { method: 'navigate', params: ['/home'] }
+     */
+    onSubmit?: Rpc;
+    /**
+     * @description the JSON Schema of the form, used for validation and defaults
+     * @see https://json-schema.org/
+     */
+    schema: Record<string, any>;
+  }>;
+
+export type MultiRecordConfig = {
+  idField: string;
+};
